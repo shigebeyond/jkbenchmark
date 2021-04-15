4554116af68c4b48bf764634f1298abbe2fa3380
shijianhang <3076679995@qq.com>
2021-04-15 18:09:07 +0800

    完善性能结果输出

--- jkbenchmark-test/src/test/kotlin/net/jkcode/jkbenchmark/tests/BenchmarkResultModelTest.kt
+++ jkbenchmark-test/src/test/kotlin/net/jkcode/jkbenchmark/tests/BenchmarkResultModelTest.kt
@@ -3,6 +3,9 @@ package net.jkcode.jkbenchmark.tests
 import net.jkcode.jkbenchmark.analyze.BenchmarkResultModel
 import net.jkcode.jkutil.common.Config
 import org.junit.Test
+import java.text.DecimalFormat
+import java.text.MessageFormat
+
 
 /**
  *
@@ -41,5 +44,61 @@ class BenchmarkResultModelTest {
                         result.create()
                     }
 
+    }
+
+
+    @Test
+    fun testFormat(){
+        //val field = "tps"
+        val field = "rt"
+
+        val actions = BenchmarkResultModel.queryBuilder()
+                .selectDistinct("action")
+                .where("requests", 100000)
+                .findColumn<String>()
+        val players = arrayOf("jkorm-druid", "jkorm-hikari", "mybatis")
+        val str = StringBuilder()
+        str.append(" | 测试场景 ")
+        for (player in players){
+            str.append("| $player $field ")
+        }
+        str.appendln(" | 性能排序 | jkorm-druid/mybatis | jkorm-hikari/mybatis | 最优 |")
+        str.appendln("|--------|-----------|---------|--------------|--------------|--------------|--------------|--------------|")
+        for (action in actions) {
+            str.append("| $action ")
+            val results = BenchmarkResultModel.queryBuilder()
+                    .where("requests", 100000)
+                    .where("action", action)
+                    .orderBy(field, field == "tps") // tps 升序, rt 降序
+                    .findModels<BenchmarkResultModel>()!!
+
+            val player2result = results.associate {
+                it.player to it
+            }
+            for(player in players) {
+                val value: Any = player2result[player]!![field]
+                str.append("| $value ")
+            }
+
+            // 排序
+            val orderPlayers = results.map {
+                it.player
+            }
+            str.append("| ").append(orderPlayers.joinToString(" > "))
+
+            // 比例
+            var druid: Double = player2result["jkorm-druid"]!![field]
+            var hikari: Double = player2result["jkorm-hikari"]!![field]
+            var mybatis: Double = player2result["mybatis"]!![field]
+            val df = DecimalFormat("0.00")
+            str.append("| " + df.format(druid * 100 / mybatis) + " % ")
+            str.append("| " + df.format(hikari * 100 / mybatis) + " % ")
+
+            str.append(" | " + results.first().player)
+
+            str.appendln("|")
+        }
+
+        println(str)
     }
 }
